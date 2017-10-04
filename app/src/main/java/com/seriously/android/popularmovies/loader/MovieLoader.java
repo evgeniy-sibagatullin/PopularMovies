179f0d4e3b43a26edca1c0291f2f6370fc97e38f
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-10-04 21:24:38 +0300

    Add cache to loader.

--- app/src/main/java/com/seriously/android/popularmovies/loader/MovieLoader.java
+++ app/src/main/java/com/seriously/android/popularmovies/loader/MovieLoader.java
@@ -7,12 +7,15 @@ import com.seriously.android.popularmovies.model.Movie;
 import com.seriously.android.popularmovies.utilities.NetworkUtils;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MovieLoader extends AsyncTaskLoader<List<Movie>> {
 
     private final Context mContext;
     private final URL mUrl;
+    private final static Map<URL, List<Movie>> cache = new HashMap<>();
 
     public MovieLoader(Context context, URL url) {
         super(context);
@@ -22,11 +25,21 @@ public class MovieLoader extends AsyncTaskLoader<List<Movie>> {
 
     @Override
     protected void onStartLoading() {
-        forceLoad();
+        if (cache.containsKey(mUrl)) {
+            deliverResult(cache.get(mUrl));
+        } else {
+            forceLoad();
+        }
     }
 
     @Override
     public List<Movie> loadInBackground() {
         return NetworkUtils.getMoviesByUrl(mUrl, mContext);
+    }
+
+    @Override
+    public void deliverResult(List<Movie> movies) {
+        cache.put(mUrl, movies);
+        super.deliverResult(movies);
     }
 }
