c950d499a6500b97832465513c39edfed98f92a9
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2018-01-22 19:35:05 +0200

    Provide placeholder and error drawables for Picasso image loader.

--- app/src/main/java/com/seriously/android/popularmovies/utilities/ImageLoader.java
+++ app/src/main/java/com/seriously/android/popularmovies/utilities/ImageLoader.java
@@ -3,19 +3,18 @@ package com.seriously.android.popularmovies.utilities;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.seriously.android.popularmovies.R;
 import com.squareup.picasso.Picasso;
 
 public class ImageLoader {
 
     private static final String TMDB_IMAGE_PATH_PREFIX = "http://image.tmdb.org/t/p/";
     private static final String W_185_SIZE_PATH = "w185/";
-    private static final String ORIGINAL_SIZE_PATH = "original/";
 
     public static void loadGridPosterImage(Context context, ImageView view, String path) {
-        Picasso.with(context).load(TMDB_IMAGE_PATH_PREFIX + W_185_SIZE_PATH + path).into(view);
-    }
-
-    public static void loadFullPosterImage(Context context, ImageView view, String path) {
-        Picasso.with(context).load(TMDB_IMAGE_PATH_PREFIX + ORIGINAL_SIZE_PATH + path).into(view);
+        Picasso.with(context).load(TMDB_IMAGE_PATH_PREFIX + W_185_SIZE_PATH + path)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.missing)
+                .into(view);
     }
 }
